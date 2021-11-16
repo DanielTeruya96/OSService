c3a1d46976a3ec4beb5605b11a3778795feb4c67
daniel.leite.dayo@gmail.com <Senha1463>
2021-11-16 18:13:33 -0400

    refatoração do ClientRequest.java

--- src/main/java/com/example/osnaruto/request/ClientRequest.java
+++ src/main/java/com/example/osnaruto/request/ClientRequest.java
@@ -20,6 +20,24 @@ public class ClientRequest {
     @ApiModelProperty(value = "Endereco do cliente", example="Rua de algum lugar numero 53")
     private String endereco;
 
+    @ApiModelProperty(value = "numero", example="2")
+    private String numero;
+
+    @ApiModelProperty(value = "cep", example="7894-000")
+    private String cep;
+
+    @ApiModelProperty(value = "Cidade", example="Nioaque")
+    private String cidade;
+
+    @ApiModelProperty(value = "Bairro", example="jardim mansão")
+    private String bairro;
+
+    @ApiModelProperty(value = "Estado", example="MS")
+    private String estado;
+
+    @ApiModelProperty(value ="inscricao", example="23543545")
+    private String inscricao;
+
     public String getRazaoSocial() {
         return razaoSocial;
     }
@@ -50,6 +68,54 @@ public class ClientRequest {
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getInscricao() {
+        return inscricao;
+    }
+
+    public void setInscricao(String inscricao) {
+        this.inscricao = inscricao;
     }
 
     @Override
