72f4f3f60597613bcfc04bf45bfbcb731dfc5b78
daniel.leite.dayo@gmail.com <Senha1463>
2021-11-16 17:57:05 -0400

    refatoração do Cliente.java

--- src/main/java/com/example/osnaruto/model/Cliente.java
+++ src/main/java/com/example/osnaruto/model/Cliente.java
@@ -17,6 +17,9 @@ public class Cliente extends EntidadeBase {
     @Column(name = "cpf_pcpj")
     private String cpfPcpj;
 
+    @Column(name = "inscricao")
+    private String inscricao;
+
     @Basic
     @Column(name = "nome_fantasia")
     private String nomeFantasia;
@@ -24,6 +27,22 @@ public class Cliente extends EntidadeBase {
     @Basic
     @Column(name = "endereco", length = 500)
     private String endereco;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "estado")
+    private String estado;
+
 
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     @Where(clause = "ativo = 1")
@@ -79,5 +98,53 @@ public class Cliente extends EntidadeBase {
     public void setContatos(List<Contato> contatos) {
         this.contatos = contatos;
 
+    }
+
+    public String getInscricao() {
+        return inscricao;
+    }
+
+    public void setInscricao(String inscricao) {
+        this.inscricao = inscricao;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
